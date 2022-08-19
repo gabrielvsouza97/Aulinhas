b24e7a1dd4aa5290860a18354c7a53d591c77eb6
Gabriel Souza <gabriel.souza@rbmweb.com.br>
2022-08-18 22:02:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    //Pegando os edits
+    $(".dinheiro").maskMoney({decimal:',', thousands:'.'});
+    $("#percent").maskMoney({precision:4,decimal:',', thousands:'.'});
+});
+
+//Logica para modificar os campos
+
+let comboModelo = document.querySelector("#calculo").value;
+
+//Verificar a cada mudança do comboBox qual o valor
+
+//Dentro da verificação, entender o que ele precisa liberar e colocar a cor para o usuario;
+
+//Bloquear e colocar cor diferente no campo que não precisa digitar
+
+//Preencher o campo com os valores referente ao combobox
+
+
+
